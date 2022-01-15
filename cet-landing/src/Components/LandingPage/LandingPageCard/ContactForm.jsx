d06da2441bcca5334c672cf7020c72c9043bf15c
Sahas <sahasvivek001@gmail.com>
2022-01-15 20:09:21 +0530

    minor bug fixes

--- cet-landing/src/Components/LandingPage/LandingPageCard/ContactForm.jsx
+++ cet-landing/src/Components/LandingPage/LandingPageCard/ContactForm.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './ContactForm.css'
+import contactimg from '../../../Assets/contactimg.png';
 
 export default function ContactForm(props) {
   return (
     <>
-      <div className='card flex flex-wrap content-center mb-24'>
+      <div className='card flex flex-wrap content-center lg:mb-48 sm:mb-24'>
         <div className='card__front'>
           <form className='card__form'>
-            <h3 className='mb-1 mt-8 text-lg font-normal'>Email</h3>
+            <h3 className='mb-1 mt-8 lg:text-lg xs:text-base font-normal'>Email</h3>
             <input className='card__inputemail mt-1 mb-4' type='email' required />
-            <h3 className='mb-1 text-lg'>Message</h3>
-            <textarea className='card__input mt-1' type='text' required />
+            <h3 className='mb-1 lg:text-lg xs:text-base mt-5'>Message</h3>
+            <textarea className='card__input mt-1 mb-8' type='text' required />
+
             <button className='card__button '>Submit</button>
           </form>
         </div>
